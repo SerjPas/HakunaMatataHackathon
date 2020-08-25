b05e4412ffe95dcdadf3e90e1042d18eab4a9766
d1m4 <dpsmnsk@gmail.com>
2020-08-25 19:15:30 +0300

    mainpage img and text added

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
+import styles from "../css/MainPage.module.css"
 import {getFarmers, getWeatherData} from '../lib/api';
 import UserTableContext from "../context/UserTableContext";
 
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image'
 import Dashboard from "./Dashboard";
 
 
@@ -11,25 +13,17 @@ const MainPage = () => {
 
     const [weatherData, setWeatherData] = useState([]);
 
-
-    //
-    // useEffect(() => {
-    //     getFarmers()
-    //         .then(response => {
-    //             console.log(response.data)
-    //             setUserTable(response.data)
-    //         });
-    //
-    //     getWeatherData()
-    //         .then(response => {
-    //             console.log(response.data)
-    //             setWeatherData(response.data)
-    //         })
-    // }, []);
-    //
-
     return (
-       <div>hello</div>
+       <Container class="cont" fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
+           <Image src="https://bountifield.org/wp-content/uploads/2019/07/Africa-Farm-Field-1-1080.png" fluid 
+            class="img-responsive img"/>
+            <div class="carousel-caption">
+              <h1>We're helping farmers in developing countries by providing accurate weather prediction 
+                  forecasts and solutions for efficient energy and water consumption based on complex data 
+                  models.
+                </h1>
+            </div>
+       </Container>
     )
 }
 
