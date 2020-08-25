3c9aea149ee52b0a62d978c6c89aaad5374f9491
Serhii <Passerhii@gmail.com>
2020-08-25 23:23:53 +0300

    css added to main page

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
+import styles from '../css/NavBar.module.css'
 
 
 const AppNavbar = () => {
@@ -9,7 +10,7 @@ const AppNavbar = () => {
   return (
     <Navbar bg="dark" expand="lg">
       <Navbar.Brand href="/" className="text-white mr-auto">Hakuna Matata</Navbar.Brand>
-      <Nav>
+      <Nav className={styles.NavBarButtons}>
         <LinkContainer to="#">
           <Nav.Link className="text-white">About</Nav.Link>
         </LinkContainer>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image'
+import styles from '../css/MainPage.module.css'
 
 
 const MainPage = () => {
 
     return (
-       <Container class="cont" fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
-           <Image src="https://bountifield.org/wp-content/uploads/2019/07/Africa-Farm-Field-1-1080.png" fluid 
-            class="img-responsive img"/>
-            <div class="carousel-caption">
-              <h1>We're helping farmers in developing countries by providing accurate weather prediction 
-                  forecasts and solutions for efficient energy and water consumption based on complex data 
-                  models.
-                </h1>
-            </div>
-       </Container>
+        <div className={styles.Body}>
+            <h1 className={styles.H1}>We're helping farmers in developing countries by providing accurate weather
+                prediction
+                forecasts and solutions for efficient energy and water consumption based on complex data
+                models.
+            </h1>
+        </div>
+
     )
 }
 
